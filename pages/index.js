c94279cef65dc533cdb3dcfed7d83bbd845675db
sujanbjit <121421155+sujanbjit@users.noreply.github.com>
2022-12-26 14:00:07 +0600

    add all file
    pages, img, sass

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+/* eslint-disable jsx-a11y/alt-text */
+import HeaderNew from "../components/headernew";
+import Story from "../components/story";
+import VideoGallary from "../components/videogallary";
+import PhotoGalary from "../components/photogalary";
+import Intro from "../components/intro";
+import IntroTopBG from "../components/introTopBg";
+import Feature from "../components/feature";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderNew />
+      <Story />
+      <PhotoGalary />
+    </>
+  );
+}
